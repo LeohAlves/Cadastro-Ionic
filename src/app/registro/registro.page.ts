e15e06d66d8f8c0f0793f6412941ef76863c7053
Leonardo Alves <leonardoalves72@hotmail.com>
2022-11-07 21:39:26 -0300

    errorMessage and add form in registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -9,12 +9,38 @@ import { FormBuilder, Validators} from '@angular/forms'
 export class RegistroPage implements OnInit {
 
   CadForm = this.formBuilder.group({
-    nome:  ['', Validators.required],
+    nome:  ['', Validators.compose([Validators.required,Validators.minLength(3)])],
     email: ['', Validators.compose([Validators.required, Validators.email])],
-    senha: ['',Validators.compose([Validators.required, Validators.minLength(6)])]
+    CPF : ['',Validators.compose([Validators.required,Validators.maxLength(11),Validators.minLength(11)])],
+    senha: ['',Validators.compose([Validators.required, Validators.minLength(8)])],
+    confirma: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
   })
+
+  errorMessage =
+  {
+    nome :[{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'minlength', aviso: 'É necessário ter no mínimo 3 caracteres.'}],
+    email : [{tipo: 'required' , aviso: 'O campo não pode estar vazio'},{ tipo: 'email', aviso: 'Email inválido'}],
+    CPF: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'minlenght', aviso: 'CPF inválido!'}, {tipo: 'maxlenght', aviso: 'CPF inválido!'}],
+    senha: [{tipo: 'required' , aviso: 'O campo não pode estar vazio'},{ tipo: 'minlength', aviso: 'É necessário ter no mínimo 8 caracteres.'}],
+    confirma: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'minlength', aviso: 'É necessário ter no mínimo 8 caracteres.'}]
+  };
   constructor( private formBuilder: FormBuilder ) { }
 
+  get nome(){
+    return this.CadForm.get('nome');
+  }
+  get email(){
+    return this.CadForm.get('email');
+  }
+  get CPF(){
+    return this.CadForm.get('CPF');
+  }
+  get senha(){
+    return this.CadForm.get('senha');
+  }
+  get confirma(){
+    return this.CadForm.get('confirma');
+  }
   ngOnInit() {
   }
 
